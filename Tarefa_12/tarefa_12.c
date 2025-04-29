d02ffa526a6fd173cbafb4a1fa168c3aec05ca4e
daniel-trindade <danielbrunotrindade@gmail.com>
2025-04-29 11:57:16 -0300

    update

---
+++ Tarefa_12/tarefa_12.c
@@ -0,0 +1,11 @@
+/*
+Avalie a escalabilidade do seu código de
+Navier-Strokes utilizando algum nó de
+computação do NPAD. Procure identificar
+gargalos de escalabilidade e reporte o
+seu progresso em versões sucessivas da
+evolução do código otimizado. Comente
+sobre a escalabilidade, a escalabilidade
+fraca e a escalabilidade fortes das
+versões
+*/
